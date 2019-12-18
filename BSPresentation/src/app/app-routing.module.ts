187704d0adc7f489ed5579c7e925dc1f70020929
carlosscarranza <58747093+carlosscarranza@users.noreply.github.com>
2019-12-18 11:35:16 -0500

    angular

--- BSPresentation/src/app/app-routing.module.ts
+++ BSPresentation/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { VisitanteComponent } from './visitante/visitante.component';
+import { SitemenuComponent } from './sitemenu/sitemenu.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "visitante", component: VisitanteComponent },
+  { path: "sitemenu", component: SitemenuComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- BSPresentation/src/app/app.module.ts
+++ BSPresentation/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VisitanteComponent } from './visitante/visitante.component';
+import { SitemenuComponent } from './sitemenu/sitemenu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VisitanteComponent,
+    SitemenuComponent
   ],
   imports: [
     BrowserModule,
